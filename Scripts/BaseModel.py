6478927df2e6ebf0e8562af23015a7dd341c6e4b
AzurAthena <AzurAthena@gmail.com>
2017-08-25 13:27:28 +0530

    siamese model

---
+++ Scripts/BaseModel.py
@@ -0,0 +1,45 @@
+from keras import backend as K
+from keras.layers.core import Activation, Dense, Dropout, Lambda
+
+
+def cosine_distance(vecs, normalize=False):
+    x, y = vecs
+    if normalize:
+        x = K.l2_normalize(x, axis=0)
+        y = K.l2_normalize(x, axis=0)
+    return K.prod(K.stack([x, y], axis=1), axis=1)
+
+
+def cosine_distance_output_shape(shapes):
+    return shapes[0]
+
+
+def sim_model(v1, v2):
+
+    print('v1', v1)
+    print('v2', v2)
+
+    merged = Lambda(cosine_distance,
+                    output_shape=cosine_distance_output_shape)([v1, v2])
+    print('merge', merged)
+    fc1 = Dense(512, kernel_initializer="glorot_uniform")(merged)
+    fc1 = Dropout(0.2)(fc1)
+    fc1 = Activation("relu")(fc1)
+
+    fc2 = Dense(128, kernel_initializer="glorot_uniform")(fc1)
+    fc2 = Dropout(0.2)(fc2)
+    fc2 = Activation("relu")(fc2)
+
+    pred = Dense(2, kernel_initializer="glorot_uniform")(fc2)
+    pred = Activation("softmax")(pred)
+    print('pred', pred)
+    return pred
+
+
+def contrastive_loss(y_true, y_pred):
+    '''Contrastive loss from Hadsell-et-al.'06
+    http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    '''
+    margin = 1
+    return K.mean(y_true * K.square(y_pred) +
+                  (1 - y_true) * K.square(K.maximum(margin - y_pred, 0)))
---
+++ Scripts/Siamese.py
@@ -0,0 +1,53 @@
+from keras.applications import VGG16
+import os
+import DataHandler as dh
+import BaseModel as bm
+from keras.models import Model
+
+DATA_DIR = os.path.abspath("../data")
+IMAGE_TEMP_DIR = os.path.join(DATA_DIR, "test")
+IMAGE_DIR = os.path.join(DATA_DIR, "holiday-photos")
+IM_SIZE = 224
+EPOCHS = 20
+BATCH_SIZE = 32
+
+print('Creating triples...')
+triples = dh.create_image_triples(IMAGE_TEMP_DIR)
+
+print('Loading images...')
+lhs, rhs, y = dh.load_image_triplets(image_dir=IMAGE_TEMP_DIR,
+                                  image_triples=triples,
+                                  image_size=IM_SIZE, shuffle=True)
+print('y', y.shape)
+print('lhs', lhs.shape)
+print('rhs', rhs.shape)
+
+vgg_1 = VGG16(weights='imagenet', include_top=True)
+vgg_2 = VGG16(weights='imagenet', include_top=True)
+
+for layer in vgg_1.layers:
+    layer.trainable = False
+    layer.name = layer.name + "_1"
+for layer in vgg_2.layers:
+    layer.trainable = False
+    layer.name = layer.name + "_2"
+print('_'*12, 'VGG16', '-'*12)
+vgg_1.summary()
+
+
+v1 = vgg_1.get_layer("flatten_1").output
+v2 = vgg_2.get_layer("flatten_2").output
+
+pred = bm.sim_model(v1, v2)
+
+model = Model(inputs=[vgg_1.input, vgg_2.input], outputs=pred)
+
+print('_'*12, 'SIAMESE', '-'*12)
+model.summary()
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit([lhs, rhs], y, epochs=EPOCHS, batch_size=BATCH_SIZE)
+# pos + pos -> 1
+# pos + neg -> 0
+
